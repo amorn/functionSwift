cb40829febc8a30a9d965d3c3edc7e1598b17ce2
admin <admin@admins-Mac-mini-309.local>
2016-05-10 09:54:07 +0700

    Strat

--- functionSwift/main.swift
+++ functionSwift/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+func hello1(){
+    print("Hello world")}
 
+func hello(kk:String="Example"){
+    print("Hello world by \(kk)")}
+
+func circleArea(ra:Float) -> Float {
+    var area:Float
+    area=3.14*ra*ra;
+    
+    return area
+}
+
+//Function return multi
+// func exParaMulti(para:type)->(min:type,max:type,sumMx:type)
+//ต้องเป็นชนิดให้ตรงกับที่เรากำหนด
+
+// Function default
+// func exParaDef(para:String="Test")
+
+func newPosition(moveRight test:Bool,xPosition:Int)->  Int {
+    func goRight(xPosition:Int)->Int{
+        return xPosition+1;
+    }
+    func goLeft(xPosition:Int)->Int{
+        return xPosition-1;
+    }
+    let dicision:Int->Int = test ? goRight:goLeft
+    return dicision(xPosition)
+}
+
+var position = newPosition(moveRight:true, xPosition: 15)
+print("Locate :\(position)")
+
+
+hello()
+hello("Amorn")
+print("Area :\(circleArea(3.5))")
